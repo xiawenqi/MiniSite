647aeee4f34dde32368c7ca94f0e7456dd373beb
xiawenqi <xiawenqi90@gmail.com>
2014-11-06 00:19:15 +0800

    首页搜索框和语言切换， 搜索框未完成， 设计稿信息不全。调整js， 把库提取出来

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+var handleLanguageSwitcher = function(){
+	$("#language-eng").on("change", function(){
+		if(this.checked){
+			$(this).parent().addClass("checked");
+		}else{
+			$(this).parent().removeClass("checked");
+		}
+	});
+};
+
+handleSearchFieldToggle = function(){
+	$(".search-btn").on("click", function(){
+		$("#searchbar").toggleClass("open");
+	});
+};
+
+var Index = function () {
+	"use strict";
+    return {
+        //main function
+        init: function () {
+        	handleLanguageSwitcher();
+        	handleSearchFieldToggle();
+        }
+    };
+}();
