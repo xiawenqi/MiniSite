955f13921c096d6685c5d82cd44ea5cb8d4d9b2e
xiawenqi <xiawenqi90@gmail.com>
2014-11-08 01:04:32 +0800

    完成coverflow

--- assets/js/form-download.js
+++ assets/js/form-download.js
@@ -1,6 +1,15 @@
 
 var handleCoverFlow = function(){
-	 $('#form-coverflow').coverflow();
+
+	 var el = $('#form-coverflow' );
+	 var select = Math.ceil(el.children().length / 2) - 1;
+	 el.coverflow({
+	 	// overlap: 0.8,
+	 	angle: 0
+	 });
+	 
+	 
+	 el.coverflow('select', select);
 };
 
 var FormDownload = function () {
